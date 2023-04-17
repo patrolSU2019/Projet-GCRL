7beb408dd1c402fb5bd27e0e44d26db529d649e8
zhenyue <zhenyuef@gmail.com>
2023-04-17 17:47:02 +0200

    cartpole goal conditioned
    Co-authored-by: qinyii <qinyii@users.noreply.github.com>

---
+++ algo/explorers/agents.py
@@ -0,0 +1,19 @@
+import torch
+from bbrl import Agent
+
+
+class EGreedyActionSelector(Agent):
+    def __init__(self, epsilon):
+        super().__init__()
+        self.epsilon = epsilon
+
+    def forward(self, t, **kwargs):
+        q_values = self.get(("q_values", t))
+        nb_actions = q_values.size()[1]
+        size = q_values.size()[0]
+        is_random = torch.rand(size).lt(self.epsilon).float()
+        random_action = torch.randint(low=0, high=nb_actions, size=(size,))
+        max_action = q_values.max(1)[1]
+        action = is_random * random_action + (1 - is_random) * max_action
+        action = action.long()
+        self.set(("action", t), action)
